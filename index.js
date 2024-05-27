d47be3b16c33a454c971211f7dda28b8fe89694e
Fahmidur Rahaman Shanto <fahmidurrahamanshanto@gmail.com>
2024-05-28 00:51:43 +0600

    created mongodb connection with the server as database

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// mongodb connection uri
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.DB_PASSWORD}@clustercrud.ctitxen.mongodb.net/?retryWrites=true&w=majority&appName=clusterCrud`;
+
+// mongodb client
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Skill Bridge Server API");
-});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
 
-app.listen(port, (req, res) => {
-  console.log(`Server is running on port: ${port}`);
-});
+    // server Home route
+    app.get("/", (req, res) => {
+      res.send("Welcome to Skill Bridge Server API");
+    });
+
+    app.listen(port, (req, res) => {
+      console.log(`Server is running on port: ${port}`);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
