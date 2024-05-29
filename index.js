172fed7784b7210620b960a5acc69be395ce9b27
Fahmidur Rahaman Shanto <fahmidurrahamanshanto@gmail.com>
2024-05-30 05:04:33 +0600

    created onsite, remote, part time, and hybrid jobs api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // mongodb connection uri
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.DB_PASSWORD}@clustercrud.ctitxen.mongodb.net/?retryWrites=true&w=majority&appName=clusterCrud`;
@@ -23,6 +23,7 @@ app.use(express.json());
 // mongodb collections
 const usersCollection = client.db("skillbridge").collection("users");
 const bannerCollection = client.db("skillbridge").collection("banner");
+const jobsCollection = client.db("skillbridge").collection("jobs");
 
 async function run() {
   try {
@@ -52,6 +53,40 @@ async function run() {
       console.log(user);
       const result = await usersCollection.insertOne(user);
       res.send(result);
+    });
+    /* 
+    --------------------------------------------------------------- */
+
+    // onsite jobs api
+    app.get("/on_site_jobs", async (req, res) => {
+      const query = { category: "On Site Job" };
+      const jobs = await jobsCollection.find(query).toArray();
+      res.send(jobs);
+    });
+
+    // Remote jobs api
+    app.get("/remote_jobs", async (req, res) => {
+      const query = { category: "Remote Job" };
+      const jobs = await jobsCollection.find(query).toArray();
+      res.send(jobs);
+    });
+    // Hybrid jobs api
+    app.get("/hybrid_jobs", async (req, res) => {
+      const query = { category: "Hybrid" };
+      const jobs = await jobsCollection.find(query).toArray();
+      res.send(jobs);
+    });
+    // Part time jobs api
+    app.get("/part_time_jobs", async (req, res) => {
+      const query = { category: "Part Time" };
+      const jobs = await jobsCollection.find(query).toArray();
+      res.send(jobs);
+    });
+
+    // job category api
+    app.get("/jobs", async (req, res) => {
+      const categories = await jobsCollection.find().toArray();
+      res.send(categories);
     });
 
     app.listen(port, (req, res) => {
